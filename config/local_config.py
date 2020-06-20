fd2502c76bb3d9339d3efb7c01ac0b765ae1a60f
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-06-20 21:39:02 +0800

    :boom: Introducing breaking changes.

--- config/local_config.py
+++ config/local_config.py
@@ -1,5 +1,5 @@
 from config.base_config import *
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "mysql://root:imooc123456@127.0.0.1/movie_cat"
+SQLALCHEMY_DATABASE_URI = "mysql://root:root@127.0.0.1/movie"
 SECRET_KEY = "root"
